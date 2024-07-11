85189b8c285663c94b7e5b6fc764210d922c3cee
isnendyankp <isnendyankp@gmail.com>
2024-07-11 19:13:40 +0700

    Add RegisterQuestion

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,3 +1,4 @@
+import RegisterQuestion from '@/components/RegisterQuestion'
 import RegisterTitle from '@/components/RegisterTitle'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const RegisterPage = () => {
   return (
     <div>
       <RegisterTitle />
+      <RegisterQuestion />
     </div>
   )
 }
