43b6c855245a0f1d0959e8af83840de63dbb418d
isnendyankp <isnendyankp@gmail.com>
2024-07-17 17:54:09 +0700

    fix style splash & login title

--- src/components/LoginTitle/index.tsx
+++ src/components/LoginTitle/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const LoginTitle = () => {
   return (
-    <div className="flex flex-col mt-[97px] ml-[56px]">
+    <div className="flex flex-col mt-[97px] ml-[] justify-center items-center">
       <div className="">
         <h1 className="text-[33px] font-[700]">Welcome,</h1>
         <h6 className="text-[19px] font-[500]">Are you ready to cook?</h6>
       </div>
     </div>
   );
-}
+};
 
-export default LoginTitle
+export default LoginTitle;
