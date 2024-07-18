3ab9d99b1271b99510f61164f927347699bf1f24
SARRA NUTRISIA <sarra.nutrisia@gmail.com>
2024-07-18 12:54:42 +0700

    Update : Aposthrope in LoginQuestion and RecipeTabs

--- src/components/RecipeTabs/index.tsx
+++ src/components/RecipeTabs/index.tsx
@@ -30,7 +30,7 @@ export default function RecipeTabs() {
           <CardHeader>
             <CardTitle>Account</CardTitle>
             <CardDescription>
-              {Make changes to your account here. Click save when you're done.}
+              Make changes to your account here. Click save when you&apos;re done.
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
@@ -53,7 +53,7 @@ export default function RecipeTabs() {
           <CardHeader>
             <CardTitle>Password</CardTitle>
             <CardDescription>
-              {Change your password here. After saving, you'll be logged out.}
+              {Change your password here. After saving, you&apos;ll be logged out.}
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
