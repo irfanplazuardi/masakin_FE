6e09cc37bd9f5a9cbe74327757aaacc280bfbe25
SARRA NUTRISIA <sarra.nutrisia@gmail.com>
2024-07-19 12:43:22 +0700

    fix : add cardrecipedetailingredients

--- src/components/RecipeDetail/RecipeDetailFirstRow.tsx
+++ src/components/RecipeDetail/RecipeDetailFirstRow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface RecipeDetailFirstRowProps {
   onIngredientsClick: () => void;
@@ -6,48 +6,85 @@ interface RecipeDetailFirstRowProps {
   onStepsClick: () => void;
 }
 
-const RecipeDetailFirstRow: React.FC<RecipeDetailFirstRowProps> = ({ onIngredientsClick, onToolsClick, onStepsClick }) => {
-  const [activeButton, setActiveButton] = useState<'ingredients' | 'tools' | 'steps' | null>(null);
+const RecipeDetailFirstRow: React.FC<RecipeDetailFirstRowProps> = ({
+  onIngredientsClick,
+  onToolsClick,
+  onStepsClick,
+}) => {
+  const [activeButton, setActiveButton] = useState<
+    "ingredients" | "tools" | "steps" | null
+  >(null);
 
   const handleIngredientsClick = () => {
-    setActiveButton('ingredients');
+    setActiveButton("ingredients");
     onIngredientsClick();
   };
 
   const handleToolsClick = () => {
-    setActiveButton('tools');
+    setActiveButton("tools");
     onToolsClick();
   };
 
   const handleStepsClick = () => {
-    setActiveButton('steps');
+    setActiveButton("steps");
     onStepsClick();
   };
 
   return (
     <div className="flex items-center justify-between w-full">
       <button
-        className={`py-2 px-4 rounded-lg flex justify-center items-center mr-2${activeButton === 'ingredients' ? 'bg-green-600 text-white' : 'text-green-600'}`}
+        className={`py-2 px-2 rounded-lg flex justify-center items-center mr-2${
+          activeButton === "ingredients"
+            ? "bg-green-600 text-white"
+            : "text-green-600"
+        }`}
         onClick={handleIngredientsClick}
-        style={{ width: '104px', height: '33px', borderRadius: '10px', backgroundColor: activeButton === 'ingredients' ? '#7E9F10' : 'transparent', color: activeButton !== 'ingredients' ? '#7E9F10' : '#FFFFFF' }}
+        style={{
+          width: "104px",
+          height: "33px",
+          borderRadius: "10px",
+          backgroundColor:
+            activeButton === "ingredients" ? "#7E9F10" : "transparent",
+          color: activeButton !== "ingredients" ? "#7E9F10" : "#FFFFFF",
+        }}
       >
         <span className="font-poppins text-xs font-semibold whitespace-nowrap">
           Bahan-bahan
         </span>
       </button>
       <button
-        className={`py-2 px-4 rounded-lg flex justify-center items-center ${activeButton === 'tools' ? 'bg-green-600 text-white' : 'text-green-600'}`}
+        className={`py-2 px-2 rounded-lg flex justify-center items-center ${
+          activeButton === "tools"
+            ? "bg-green-600 text-white"
+            : "text-green-600"
+        }`}
         onClick={handleToolsClick}
-        style={{ width: '106px', height: '33px', borderRadius: '10px', backgroundColor: activeButton === 'tools' ? '#7E9F10' : 'transparent', color: activeButton !== 'tools' ? '#7E9F10' : '#FFFFFF' }}
+        style={{
+          width: "106px",
+          height: "33px",
+          borderRadius: "10px",
+          backgroundColor: activeButton === "tools" ? "#7E9F10" : "transparent",
+          color: activeButton !== "tools" ? "#7E9F10" : "#FFFFFF",
+        }}
       >
         <span className="font-poppins text-xs font-semibold whitespace-nowrap">
           Alat-alat
         </span>
       </button>
       <button
-        className={`py-2 px-4 rounded-lg flex justify-center items-center ${activeButton === 'steps' ? 'bg-green-600 text-white' : 'text-green-600'}`}
+        className={`py-2 px-2 rounded-lg flex justify-center items-center ${
+          activeButton === "steps"
+            ? "bg-green-600 text-white"
+            : "text-green-600"
+        }`}
         onClick={handleStepsClick}
-        style={{ width: '105px', height: '33px', borderRadius: '10px', backgroundColor: activeButton === 'steps' ? '#7E9F10' : 'transparent', color: activeButton !== 'steps' ? '#7E9F10' : '#FFFFFF' }}
+        style={{
+          width: "105px",
+          height: "33px",
+          borderRadius: "10px",
+          backgroundColor: activeButton === "steps" ? "#7E9F10" : "transparent",
+          color: activeButton !== "steps" ? "#7E9F10" : "#FFFFFF",
+        }}
       >
         <span className="font-poppins text-xs font-semibold whitespace-nowrap">
           Cara Masak
--- src/components/RecipeDetail/index.tsx
+++ src/components/RecipeDetail/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import RecipeDetailFirstRow from './RecipeDetailFirstRow';
-import RecipeDetailSecondRow from './RecipeDetailSecondRow';
-import CardRecipeDetailIngredients from './CardRecipeDetailIngredients';
+import React, { useState } from "react";
+import RecipeDetailFirstRow from "./RecipeDetailFirstRow";
+import RecipeDetailSecondRow from "./RecipeDetailSecondRow";
+import CardRecipeDetailIngredients from "./CardRecipeDetailIngredients";
 
 interface RecipeDetailProps {
   showIngredients: boolean;
@@ -12,14 +12,48 @@ interface RecipeDetailProps {
   onStepsClick: () => void;
 }
 
-const RecipeDetail: React.FC<RecipeDetailProps> = ({ onIngredientsClick, onToolsClick, onStepsClick }) => {
+const RecipeDetail: React.FC<RecipeDetailProps> = ({
+  showIngredients,
+  showTools,
+  showSteps,
+  onIngredientsClick,
+  onToolsClick,
+  onStepsClick,
+}) => {
   const [showIngredientsDetail, setShowIngredientsDetail] = useState(true); // Set initial state to true
 
   return (
-    <div className="bg-white py-4 px-6 flex flex-col items-center justify-between" style={{ width: '324px', margin: '0 auto', gap: '10px', borderRadius: '10px 0px 0px 0px' }}>
-      <RecipeDetailFirstRow onIngredientsClick={onIngredientsClick} onToolsClick={onToolsClick} onStepsClick={onStepsClick} />
+    <div
+      className="bg-white py-2 flex flex-col items-center justify-between"
+      style={{
+        width: "324px",
+        margin: "0 auto",
+        gap: "10px",
+        borderRadius: "10px 0px 0px 0px",
+      }}
+    >
+      <RecipeDetailFirstRow
+        onIngredientsClick={onIngredientsClick}
+        onToolsClick={onToolsClick}
+        onStepsClick={onStepsClick}
+      />
       {showIngredientsDetail && <RecipeDetailSecondRow />}
-      <CardRecipeDetailIngredients ingredient={''} />
+      <div className="space-y-2">
+        {" "}
+        {/* Add spacing between the buttons */}
+        <CardRecipeDetailIngredients
+          leftText="Bahan Utama 1"
+          rightText="100 gram"
+        />
+        <CardRecipeDetailIngredients
+          leftText="Bahan Utama 2"
+          rightText="200 gram"
+        />
+        <CardRecipeDetailIngredients
+          leftText="Bahan Utama 3"
+          rightText="200 gram"
+        />
+      </div>
     </div>
   );
 };
